ab965f3d81c5f5df64309cbc1edc40fcbeb4973c
Michael Harris <mharris@lbisoftware.com>
2017-12-04 11:57:41 -0500

    More accurate solution to Day 3 - Part 1

--- src/twentyseventeen/Day3Part1.java
+++ src/twentyseventeen/Day3Part1.java
@@ -3,18 +3,39 @@ package twentyseventeen;
 public class Day3Part1 {
 	
 	public static void main(String[] args) {
+		int input = 312051;
 		int layer = 0;
 		int cornerNum = 1;
 		
 		//Determine what layer number our input value is on
-		while (cornerNum < 312051) {
+		while (cornerNum < input) {
 			layer++;
-			cornerNum = ((2 * layer) + 1) * ((2 * layer) + 1);
+			cornerNum += (8 * layer);
 		}
 		
-		System.out.println("Corner Num is: " + cornerNum);
-		System.out.println("Distance from center is: " + (cornerNum - 312051));
+		int faceSize = (layer * 8) / 4;
+		int lastCornerNum = cornerNum - faceSize;
+		int midpoint = cornerNum - (faceSize / 2);
 		
+		System.out.println("Corner Num is: " + cornerNum);
+		System.out.println("Previous corner Num is: " + lastCornerNum);
+		System.out.println("Midpoint is: " + midpoint);
+		
+		int answer = 0;
+		if (midpoint == input) {
+			answer = layer;
+		}
+		else if (cornerNum == input) {
+			answer = layer * 2;
+		}
+		else if (midpoint < input) {
+			answer = layer + (input - midpoint);
+		}
+		else if (midpoint > input) {
+			answer = layer + (midpoint - input);
+		}
+		
+		System.out.println("Distance from center is: " + answer);
 	}
 	
 }
